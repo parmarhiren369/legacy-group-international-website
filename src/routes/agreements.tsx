258439eeaf62f053f803b3c68102b94991ca20a6
hirenparmar <parmarhiren36911@gmail.com>
2026-08-13 12:16:31 +0530

    Update agreements.tsx

--- src/routes/agreements.tsx
+++ src/routes/agreements.tsx
@@ -1,1 +1,6 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { Agreements } from "@/components/site/Agreements";
 
+export const Route = createFileRoute("/agreements")({
+  component: Agreements,
+});
