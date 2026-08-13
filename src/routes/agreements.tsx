97d4aadc5455c77770a89334b328931c460875c8
hirenparmar <parmarhiren36911@gmail.com>
2026-08-13 12:21:44 +0530

    Fix import statement for Agreements component

--- src/routes/agreements.tsx
+++ src/routes/agreements.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import Agreements from "@/components/site/Agreements";
+import { Agreements } from "@/components/site/Agreements";
 
 export const Route = createFileRoute("/agreements")({
   component: Agreements,
